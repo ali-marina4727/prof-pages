9b99d4cfb43dcb9920ab158b51bd6da2b90873ae
Марина <64268387+ali-marina4727@users.noreply.github.com>
2021-02-13 18:59:33 +0300

    Add files via upload

---
+++ AddTask.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input v-model="newTask" type="text" placeholder="Добавь задачу" />
+    <button @click="addSomeTask" @onAddTask="task = $event">добавить</button>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "./main.js";
+
+export default {
+  data() {
+    return {
+      nextId: 3,
+      newTask: "",
+    };
+  },
+  props: {
+    task: {
+      type: String,
+      default: "d name",
+    },
+  },
+  methods: {
+    addSomeTask() {
+      if (this.newTask) {
+        eventBus.$emit("onAddTask", this.newTask, this.nextId);
+        this.newTask = "";
+        this.nextId++;
+      }
+      console.log(this.nextId);
+    },
+  },
+};
+</script>
---
+++ TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class=".todolist">
+    <li :key="task" v-for="task in tasks">{{ task.id }}. {{task.title}}</li>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "./main.js";
+
+export default {
+  data() {
+    return {
+      tasks: [
+        { title: "поесть", id: 1 },
+        { title: "попить", id: 2 },
+      ],
+    };
+  },
+
+  props: {
+    task: {
+      type: String,
+    },
+  },
+  created() {
+    eventBus.$on("onAddTask", (title, id) => {
+      this.tasks.push({ title: title, id: id });
+    });
+  },
+  methods: {},
+};
+</script>
